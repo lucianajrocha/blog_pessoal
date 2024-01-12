019d9a557ef7d605ac7ab2e513344e8ede87a68d
Luciana Rocha <lucianajrocha97@gmail.com>
2024-01-11 22:19:06 -0300

    feat: implement getById

--- src/main/java/com/example/blog_pessoal/controller/PostagemController.java
+++ src/main/java/com/example/blog_pessoal/controller/PostagemController.java
@@ -4,9 +4,11 @@ package com.example.blog_pessoal.controller;
 import com.example.blog_pessoal.model.Postagem;
 import com.example.blog_pessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
@@ -15,15 +17,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "+", allowedHeaders = "+")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
     @Autowired
     private PostagemRepository postagemRepository;
 
     @GetMapping
-    public ResponseEntity<List<Postagem>> gelAll(){
+    public ResponseEntity<List<Postagem>> getAll(){
         return ResponseEntity.ok(postagemRepository.findAll());
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+        return postagemRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+
 }
