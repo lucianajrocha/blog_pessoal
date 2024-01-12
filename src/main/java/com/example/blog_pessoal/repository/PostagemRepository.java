aded0a6635b33dcd4da6d8c37987498a10ea469c
Luciana Rocha <lucianajrocha97@gmail.com>
2024-01-12 16:31:49 -0300

    implementando o blog

--- src/main/java/com/example/blog_pessoal/repository/PostagemRepository.java
+++ src/main/java/com/example/blog_pessoal/repository/PostagemRepository.java
@@ -2,6 +2,11 @@ package com.example.blog_pessoal.repository;
 
 import com.example.blog_pessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository <Postagem, Long>  {
+
+    List<Postagem>findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+// SELECT * FROM tb_postagens where titulo like %titulo%;
 }
